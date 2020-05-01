c2be57c5fa9a3aa55957b5392b6ddaa26afa1bdc
DmitryShahbazov <dshakhbazov@stch.ru>
2020-05-01 22:34:20 +0300

    read vpn log

--- main.py
+++ main.py
@@ -4,9 +4,15 @@ from telegram.ext import Updater, CommandHandler
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 
+def read_vpn_status_log():
+    with open('/etc/openvpn/server/openvpn-status.log', 'r') as log_file:
+        log_text = log_file.read()
+        log_file.close()
+    return log_text
+
+
 def get_vpn_status(update, context):
-    update.message.reply_text(
-        'Hello {}'.format(update.message.from_user.first_name))
+    update.message.reply_text(read_vpn_status_log())
 
 
 updater = Updater('1159891259:AAFNZ3isJAWyH8C2dUyfgGfah5yET04fi84', use_context=True)
