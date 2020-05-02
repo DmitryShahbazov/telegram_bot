f0044e3ab94c866e150a77d5e351b0acec17eb41
DmitryShahbazov <dshakhbazov@stch.ru>
2020-05-02 13:32:03 +0300

    redis fix

--- redis_client/redis_client.py
+++ redis_client/redis_client.py
@@ -17,6 +17,5 @@ class RedisClient:
         update_data = tg.api_get_updates()
         for data in update_data['result']:
             self.redis_client.set(data['update_id'], str(data), nx=True)
-            return True
-        return False
+        return update_data['result']
 
